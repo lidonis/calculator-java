4bfc20cea5d7adcdb115b78426647a5ff9b6d0d1
lidonis <lidonis.calhau@gmail.com>
2018-05-21 14:23:39 +0200

    test print stream

--- src/main/java/fr/lidonis/calculator/CalculatorCLI.java
+++ src/main/java/fr/lidonis/calculator/CalculatorCLI.java
@@ -2,12 +2,14 @@ package fr.lidonis.calculator;
 
 import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
+import java.util.Objects;
 
 class CalculatorCLI {
 
     private final PrintStream outContent;
 
     CalculatorCLI(PrintStream outContent) {
+        Objects.requireNonNull(outContent);
         this.outContent = outContent;
     }
 
--- src/test/java/fr/lidonis/calculator/CalculatorCLITest.java
+++ src/test/java/fr/lidonis/calculator/CalculatorCLITest.java
@@ -6,9 +6,16 @@ import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.catchThrowable;
 import static org.junit.jupiter.api.Assertions.*;
 
 class CalculatorCLITest {
+
+    @Test
+    void given_null_print_stream_it_should_throw_npe() {
+        Throwable throwable = catchThrowable(() -> new CalculatorCLI(null));
+        assertThat(throwable).isInstanceOf(NullPointerException.class);
+    }
 
     @Test
     void given_1_plus_1_it_should_print_2() {
@@ -18,5 +25,4 @@ class CalculatorCLITest {
         assertThat(evaluate).isEqualTo(0);
         assertThat(outContent.toString()).isEqualTo("2");
     }
-
 }
