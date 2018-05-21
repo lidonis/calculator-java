09ad43bde87ffb9aa6eb33b38723433e59d15268
lidonis <lidonis.calhau@gmail.com>
2018-05-21 16:08:40 +0200

    rename test

--- src/test/java/fr/lidonis/calculator/CalculatorImplTest.java
+++ src/test/java/fr/lidonis/calculator/CalculatorImplTest.java
@@ -9,13 +9,13 @@ import static org.assertj.core.api.Assertions.assertThat;
 class CalculatorImplTest {
 
     @Test
-    void given_1_plus_1_should_return_2() {
+    void given_1_plus_1_return_2() {
         BigDecimal result = new CalculatorImpl().evaluate("1+1");
         assertThat(result).isEqualTo(new BigDecimal(2));
     }
 
     @Test
-    void given_5_plus_10_should_return_2() {
+    void given_5_plus_10_return_2() {
         BigDecimal result = new CalculatorImpl().evaluate("5+10");
         assertThat(result).isEqualTo(new BigDecimal(15));
     }
